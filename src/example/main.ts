33168123f31a1a46fad327ee7383d04f3c655937
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-04-15 22:10:01 +0900

    feat(example): Add example for getEvents().

--- src/example/main.ts
+++ src/example/main.ts
@@ -1,30 +1,51 @@
-import { ConnpassV1 } from '../v1'
+import { Connpass, ConnpassEvent, EventOrder } from '..'
+import { Prefecture } from '../types/prefecture'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <button id="fetchButton">Get Event List.</button>
-  <div id="result"></div>
+  <div>
+    <h3>イベント一覧</h3>
+    <button id="fetchButton">取得</button>
+    <div id="result"></div>
+  </div>
 `
-const connpass = new ConnpassV1()
+
+const apiKey = import.meta.env.VITE_CONNPASS_API_KEY
+const connpass = new Connpass(apiKey)
 
 document.getElementById('fetchButton')?.addEventListener('click', async () => {
-  const response = await connpass.getEventsV1({ count: 3 })
+  const response = await connpass.getEvents({
+    keyword: ['python', 'go'],
+    prefecture: [Prefecture.OSAKA, Prefecture.TOKYO],
+    order: EventOrder.STARTED_AT,
+    count: 3,
+  })
   const events = response.events
 
   const tableHtml = `
     <table border="1" cellpadding="5">
       <thead>
         <tr>
-          <th>Title</th>
+          <th>イベントID</th>
+          <th>イベント名</th>
+          <th>開催日時</th>
+          <th>イベント画像</th>
           <th>URL</th>
         </tr>
       </thead>
       <tbody>
         ${events
           .map(
-            (event: any) => `
+            (event: ConnpassEvent) => `
           <tr>
+            <td>${event.id}</td>
             <td>${event.title}</td>
-            <td><a href="${event.event_url}" target="_blank">Link</a></td>
+            <td>${event.started_at}</td>
+            <td>
+              <img src="${event.image_url}" width="100"></img>
+            </td>
+            <td>
+              <a href="${event.url}" target="_blank">リンク</a>
+            </td>
           </tr>
         `,
           )
